5daa5cb4b7d06a30f9f4a2dadc04582774e315a0
H3wastooshort <hacker3000@posteo.org>
2023-09-22 22:03:52 +0200

    works

--- RadInterval/Program.cs
+++ RadInterval/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using LD.Api;
 using LD.Api.IOs;
@@ -14,31 +15,36 @@ namespace RadInterval
             using (CASSYIOs cassyios = new CASSYIOs())
             {
                 cassy.Connection = Connection.Usb;
-                Console.WriteLine("Plug in Mobile-CASSY 2 now.");
-                while (!cassy.Open()) ; ;
+                if (!cassy.Open()) { Console.WriteLine("Could not open CASSY. Is it plugged in and not used by another program?"); return -1; }
                 
                 if (cassy.CASSYType == CASSYTypes.Mobile2) Console.WriteLine("Mobile-CASSY 2 found.");
-                else { Console.WriteLine("Mobile-CASSY 2 not found."); return -1; }
+                else { Console.WriteLine("Mobile-CASSY 2 not found."); return -2; }
 
                 Box524440 gmBox = new Box524440(cassy, 0);
                 cassyios.Add(gmBox);
                 cassyios.Scan(ScanMode.ScanOnly);
-                if (!gmBox.SensorBoxValid) { Console.WriteLine("Missing GM Box on input A"); return -2; }
+                if (!gmBox.SensorBoxValid) { Console.WriteLine("Missing GM Box on input A."); return -3; }
                 CASSYQuantity clicksQ = gmBox.QuantityN;
                 clicksQ.Selected = true;
+
                 double lastN = 0;
-                double lastT = 0;
+                Stopwatch clock = new Stopwatch(); //bad idea, bad and hacky, dont do this
+                clock.Start();
                 using (StreamWriter logfile = new StreamWriter("rad_intervals.csv")) while (true)
                     {
                         cassyios.DoSingleMeasurement();
                         if (clicksQ.Value > lastN)
                         {
-                            double timeNow = cassyios.Times[0]; //hacky and bad
-                            double deltaT = timeNow - lastT;
-                            lastT = timeNow;
+                            clock.Stop();
+
                             lastN = clicksQ.Value;
+
+                            double deltaT = clock.Elapsed.TotalMilliseconds;
                             Console.WriteLine(deltaT);
                             logfile.WriteLine(deltaT);
+                            
+
+                            clock.Restart();
                         }
                     }
             }
